010503d82d4aaed181e1432ec166bd69685c88b6
hrabcak <hrabcak@d0b4f782-2c5c-11df-af82-b5ce7a3b21f7>
2008-03-11 09:23:22 +0000

    first iteration gcc atomic...

--- atomic/atomic_gcc.h
+++ atomic/atomic_gcc.h
@@ -1,6 +1,90 @@
 #ifndef __COMM_ATOMIC_GCC_H__
 #define __COMM_ATOMIC_GCC_H__
 
+#include "../commtypes.h"
+#include "../assert.h"
 
+namespace atomic {
+	typedef coid::int32 aint32;
+	typedef coid::int64 aint64;
+
+	inline aint32 inc(volatile aint32 * ptr)
+	{
+		DASSERT(sizeof(aint32) == 4 && sizeof(long) == 4);
+		return __sync_add_and_fetch(ptr, static_cast<aint32>(1));
+	}
+
+	inline aint32 dec(volatile aint32 * ptr)
+	{
+		DASSERT(sizeof(aint32) == 4 && sizeof(long) == 4);
+		return __sync_sub_and_fetch(ptr, static_cast<aint32>(1));
+	}
+
+	inline aint32 cas(
+		aint32 * ptr, const aint32 val, const aint32 cmp) 
+	{
+		DASSERT(sizeof(aint32) == 4 && sizeof(long) == 4);
+
+		return 0;/*_InterlockedCompareExchange(
+			reinterpret_cast<long volatile*>(ptr), val, cmp);*/
+	}
+
+	inline bool b_cas(
+		aint32 * ptr, const aint32 val, const aint32 cmp) 
+	{
+		DASSERT(sizeof(aint32) == 4 && sizeof(long) == 4);
+
+		return false;/*_InterlockedCompareExchange(
+			reinterpret_cast<long volatile*>(ptr), val, cmp) == cmp;*/
+	}
+
+	inline aint64 cas(
+		aint64 * ptr, const aint64 val, const aint64 cmp) 
+	{
+		DASSERT(sizeof(aint64) == 8 && sizeof(__int64) == 8);
+
+		return 0;/*_InterlockedCompareExchange64(
+			reinterpret_cast<__int64 volatile*>(ptr), val, cmp);*/
+	}
+
+	inline bool b_cas(
+		aint64 * ptr, const aint64 val, const aint64 cmp) 
+	{
+		DASSERT(sizeof(aint64) == 8 && sizeof(__int64) == 8);
+
+		return false;/*_InterlockedCompareExchange64(
+			reinterpret_cast<__int64 volatile*>(ptr), val, cmp) == cmp;*/
+	}
+
+#ifdef SYSTYPE_64
+	inline bool b_cas(
+		aint64 * ptr, const aint64 valh, const aint64 vall, aint64 * cmp) 
+	{
+		DASSERT(sizeof(aint64) == 8 && sizeof(__int64) == 8);
+
+		return _InterlockedCompareExchange128(
+			reinterpret_cast<__int64 volatile*>(ptr), 
+			valh, vall, 
+			cmp) == cmp;
+	}
+#endif
+
+	inline void * cas_ptr(void * * pdst, void * pval, void * pcmp) 
+	{
+		return 0;/*reinterpret_cast<void*>(_InterlockedCompareExchange(
+			reinterpret_cast<long volatile *>(pdst),
+			reinterpret_cast<long>(pval),
+			reinterpret_cast<long>(pcmp)));*/
+	}
+
+	inline bool b_cas_ptr(void * * pdst, void * pval, void * pcmp) 
+	{
+		return false;/*_InterlockedCompareExchange(
+			reinterpret_cast<long volatile *>(pdst),
+			reinterpret_cast<long>(pval),
+			reinterpret_cast<long>(pcmp)) == reinterpret_cast<long>(pcmp);*/
+	}
+
+} // end of namespace atomic
 
 #endif // __COMM_ATOMIC_GCC_H__
