24af107580097f031a8607a38d1f309662372afc
cameni <cameni@d0b4f782-2c5c-11df-af82-b5ce7a3b21f7>
2008-04-25 10:33:43 +0000

    atomic_test moved to comm/comm_test. fmtstreamxml2 produces usable xml output. Fixed metastream handling of nameless root.

--- trunk/src/coid/comm/comm_test/main.cpp
+++ trunk/src/coid/comm/comm_test/main.cpp
@@ -5,9 +5,13 @@ void test();
 void metastream_test();
 }
 
+int main_atomic(int argc, char * argv[]);
 
+
+////////////////////////////////////////////////////////////////////////////////
 int main( int argc, char* argv[] )
 {
+    //main_atomic(argc, argv);
     //coid::test();
     coid::metastream_test();
     return 0;
