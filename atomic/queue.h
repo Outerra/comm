ae2712687d568aa5f23ec6470afa961fe4458ca2
hrabcak <hrabcak@d0b4f782-2c5c-11df-af82-b5ce7a3b21f7>
2008-10-08 20:25:54 +0000

    minor change in atomic queue...

--- atomic/queue.h
+++ atomic/queue.h
@@ -16,28 +16,27 @@ public:
 	//! helper pointer with tag
 	struct ptr_t
 	{
-		explicit ptr_t(node * const p) : ptr(p) , tag(0) {}
+		explicit ptr_t(node * const p) : _ptr(p) , _tag(0) {}
 
-		ptr_t(node * const p, const unsigned int t) : ptr(p), tag(t) {}
+		ptr_t(node * const p, const int32 t) : _ptr(p), _tag(t) {}
 
-		ptr_t() : ptr(0) , tag(0) {}
+		ptr_t() : _ptr(0) , _tag(0) {}
+
+		void set(node * const p, const int32 t) { _ptr = p; _tag = t; }
 
 		bool operator == (const ptr_t & p)
 		{
-			return (ptr == p.ptr) && (tag == p.tag);
+			return (_ptr == p._ptr) && (_tag == p._tag);
 		}
 
-		bool operator != (const ptr_t & p)
-		{
-			return !operator == (p);
-		}
+		bool operator != (const ptr_t & p) { return !operator == (p); }
 
 		union {
 			struct {
-				node * volatile ptr;
-				volatile unsigned int tag;
+				node * volatile _ptr;
+				volatile unsigned int _tag;
 			};
-			volatile int64 data;
+			volatile int64 _data;
 		};
 	};
 
@@ -70,29 +69,29 @@ protected:
 protected:
 
 	//! optimistic fix called when prev pointer is not set
-	void fixList(node_ptr_t tail, node_ptr_t head)
+	void fixList(node_ptr_t & tail, node_ptr_t & head)
 	{
 		node_ptr_t curNode, curNodeNext, nextNodePrev;
 
 		curNode = tail;
 
 		while ((head == _head) && (curNode != head)) {
-			curNodeNext = curNode.ptr->_next;
+			curNodeNext = curNode._ptr->_next;
 
-			if (curNodeNext.tag != curNode.tag)
+			if (curNodeNext._tag != curNode._tag)
 				return;
 
-			nextNodePrev = curNodeNext.ptr->_prev;
-			if (nextNodePrev != node_ptr_t(curNode.ptr, curNode.tag - 1))
-				curNodeNext.ptr->_prev = node_ptr_t(curNode.ptr, curNode.tag - 1);
+			nextNodePrev = curNodeNext._ptr->_prev;
+			if (nextNodePrev != node_ptr_t(curNode._ptr, curNode._tag - 1))
+				curNodeNext._ptr->_prev.set(curNode._ptr, curNode._tag - 1);
 
-			curNode = node_ptr_t(curNodeNext.ptr, curNode.tag - 1);
+			curNode.set(curNodeNext._ptr, curNode._tag - 1);
 		};
 	}
 
 public:
 	//!	constructor
-	queue() : _tail(new node_t(true)) , _head(_tail.ptr) , _dpool() {}
+	queue() : _tail(new node_t(true)) , _head(_tail._ptr) , _dpool() {}
 
 	//!	destructor (do not clear queue for now)
 	~queue() 
@@ -108,13 +107,13 @@ public:
 
 		node_t * const newnode = item;
 
-		newnode->_prev = node_ptr_t(0, 0);
+		newnode->_prev.set(0, 0);
 
 		for (;;) {
 			tail = _tail;
-			newnode->_next = node_ptr_t(tail.ptr, tail.tag + 1);
-			if (b_cas(&_tail.data, node_ptr_t(newnode, tail.tag + 1).data, tail.data)) {
-				tail.ptr->_prev = node_ptr_t(newnode, tail.tag);
+			newnode->_next.set(tail._ptr, tail._tag + 1);
+			if (b_cas(&_tail._data, node_ptr_t(newnode, tail._tag + 1)._data, tail._data)) {
+				tail._ptr->_prev.set(newnode, tail._tag);
 				return;
 			}
 		}
@@ -130,9 +129,9 @@ public:
 			head = _head;
 			tail = _tail;
 			if (head == _head) {
-				if (!head.ptr->_dummy) {
+				if (!head._ptr->_dummy) {
 					if (tail != head) {
-						if (head.ptr->_prev.tag != head.tag) {
+						if (head._ptr->_prev._tag != head._tag) {
 							fixList(tail, head);
 							continue;
 						}
@@ -140,26 +139,26 @@ public:
 					else {
 						dummy = _dpool.pop();
 						if (dummy == 0) dummy = new node_t(true);
-						dummy->_next = node_ptr_t(tail.ptr, tail.tag + 1);
-						if (b_cas(&_tail.data, node_ptr_t(dummy, tail.tag + 1).data, tail.data))
-							head.ptr->_prev = node_ptr_t(dummy, tail.tag);
+						dummy->_next.set(tail._ptr, tail._tag + 1);
+						if (b_cas(&_tail._data, node_ptr_t(dummy, tail._tag + 1)._data, tail._data))
+							head._ptr->_prev.set(dummy, tail._tag);
 						else
 							_dpool.push(dummy);
 						continue;
 					}
-					if (b_cas(&_head.data, node_ptr_t(head.ptr->_prev.ptr, head.tag + 1).data, head.data))
-						return static_cast<T*>(head.ptr);
+					if (b_cas(&_head._data, node_ptr_t(head._ptr->_prev._ptr, head._tag + 1)._data, head._data))
+						return static_cast<T*>(head._ptr);
 				} 
 				else {
-					if (tail.ptr == head.ptr)
+					if (tail._ptr == head._ptr)
 						return 0;
 					else {	
-						if (head.ptr->_prev.tag != head.tag) {
+						if (head._ptr->_prev._tag != head._tag) {
 							fixList(tail, head);
 							continue;
 						}
-						if (b_cas(&_head.data, node_ptr_t(head.ptr->_prev.ptr, head.tag + 1).data, head.data))
-							_dpool.push(head.ptr);
+						if (b_cas(&_head._data, node_ptr_t(head._ptr->_prev._ptr, head._tag + 1)._data, head._data))
+							_dpool.push(head._ptr);
 					}
 				} 
 			}
