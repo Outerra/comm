30592468b8b4cb16a2064d4471efc726b31170c3
hrabcak <hrabcak@d0b4f782-2c5c-11df-af82-b5ce7a3b21f7>
2009-01-05 20:06:57 +0000

    a few changes in ref stack and queue...

---
+++ atomic/pool.h
@@ -0,0 +1,63 @@
+#ifndef __COMM_ATOMIC_POOL_H__
+#define __COMM_ATOMIC_POOL_H__
+
+#include "stack.h"
+#include "queue.h"
+#include "../ref.h"
+
+template<class P>
+class pool
+{
+protected:
+	atomic::stack<P> _stack;
+
+public:
+	//! static instance of stack
+	static pool & get() { static pool p; return p; }
+
+	//! create instance or take one from pool
+	P* create() {
+		P* p = _stack.pop();
+		if( p!=0 ) {
+			p->add_ref_copy();
+			return p;
+		}
+		else {
+			return P::internal_create(this);
+		}
+	}
+
+	void destroy( P* p) {
+		//!TODO call reset?
+		p->object_ptr()->reset();
+		_stack.push(p);
+	}
+};
+
+template<class T>
+class policy_queue_pooled 
+	: public policy_ref_count<T>
+	, public atomic::stack_node
+	, public atomic::queue_node
+{
+	typedef policy_queue_pooled<T> this_t;
+	typedef pool<this_t> pool_t;
+
+protected:
+	explicit policy_queue_pooled(T* const obj, pool_t* const pl=0) 
+		: policy_ref_count(obj)
+		, _pool(pl) {}
+
+public:
+	virtual void destroy() { DASSERT(_pool!=0); _pool->destroy(this); }
+
+	static this_t* create() { return SINGLETON(pool_t).create(); }
+
+	static this_t* create(pool_t* p) { DASSERT(p!=0); return p->create(); }
+
+	static this_t* internal_create(pool_t* pl) { return new this_t(new T,pl); }
+
+	pool_t* _pool;
+};
+
+#endif // __COMM_ATOMIC_POOL_H__
--- atomic/stack.h
+++ atomic/stack.h
@@ -40,37 +40,26 @@
 
 namespace atomic {
 
-template<class T>
 struct stack_node
 {
 	stack_node() : _nexts(0) {}
 
-	T * volatile _nexts;
+	stack_node* volatile _nexts;
 };
 
 template <class T>
-struct DefaultSelector
-{
-	static stack_node<T>* get(T * const p) { return static_cast<stack_node<T>*>(p); }
-};
-
-template <class T, class S = DefaultSelector<T> >
 class stack
 {
-public:
-	typedef stack_node<T> node_t;
-
 private:
 	struct ptr_t
 	{
 		ptr_t() : _ptr(0), _pops(0) {}
 
-		ptr_t(T * const ptr, const uint pops)
-			: _ptr(ptr), _pops(pops) {}
+		ptr_t(stack_node* const ptr, const uint pops) : _ptr(ptr), _pops(pops) {}
 
 		union {
 			struct {
-		        T * volatile _ptr;
+		        stack_node * volatile _ptr;
 			    volatile uint _pops;
 			};
 			volatile int64 _data;
@@ -84,12 +73,11 @@ public:
 
 	void push(T * item)
 	{
+		stack_node* node=item;
+
 		for (;;) {
-			S::get(item)->_nexts = _head._ptr;
-			if (b_cas_ptr(
-			    reinterpret_cast<void*volatile*>(&_head._ptr),
-			    item, 
-			    S::get(item)->_nexts))
+			node->_nexts = _head._ptr;
+			if (b_cas_ptr( reinterpret_cast<void*volatile*>(&_head._ptr),node,node->_nexts ) )
 				break;
 		}
 	}
@@ -97,13 +85,13 @@ public:
 	T * pop()
 	{
 		for (;;) {
-			const ptr_t head = _head;
+			const ptr_t head=_head;
 
-			if (head._ptr == 0) return 0;
+			if( head._ptr==0 ) return 0;
 
-			const ptr_t next(S::get(head._ptr)->_nexts, head._pops + 1);
+			const ptr_t next( head._ptr->_nexts, head._pops+1 );
 
-			if (b_cas(&_head._data, next._data, head._data)) {
+			if( b_cas( &_head._data, next._data, head._data ) ) {
 				return static_cast<T*>(head._ptr);
 			}
 		}
