9ef8d8f5b8ec89839a653746b6f0d5f7ac122756
hrabcak <hrabcak@d0b4f782-2c5c-11df-af82-b5ce7a3b21f7>
2009-11-23 19:46:16 +0000

    fixed enum_type

--- trunk/src/coid/comm/binstream/binstream.h
+++ trunk/src/coid/comm/binstream/binstream.h
@@ -1044,7 +1044,7 @@ template<> struct EnumType<8>       { typedef int64    TEnum; };
 template<> struct EnumType<2>       { typedef short    TEnum; };
 template<> struct EnumType<1>       { typedef char     TEnum; };
 
-#define ENUM_TYPE(x)  (*(EnumType<sizeof(x)>::TEnum*)(void*)&x)
+#define ENUM_TYPE(x)  (*(coid::EnumType<sizeof(x)>::TEnum*)(void*)&x)
 
 ////////////////////////////////////////////////////////////////////////////////
 struct opcd_formatter
