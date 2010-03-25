5a0ce9d8342d8b838bc85cb800e544526a916207
cameni <cameni@d0b4f782-2c5c-11df-af82-b5ce7a3b21f7>
2010-03-25 23:46:19 +0000

    metastream: default value fix

--- trunk/src/coid/comm/metastream/metastream.h
+++ trunk/src/coid/comm/metastream/metastream.h
@@ -1832,7 +1832,7 @@ protected:
         opcd e=0;
         if( cache_prepared() )
         {
-            if(R && !_cachevar && !_current->valid_addr()) {
+            if(R && !t.is_array_end() && !_cachevar && !_current->valid_addr()) {
             //cache is open for reading but the member is not there
             //this can happen when reading a struct that was cached due to reordered input
                 if( !cache_use_default() ) {
