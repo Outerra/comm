d527b382e1e414a5837ab3e2dd9ef87e88e09682
cameni <cameni@d0b4f782-2c5c-11df-af82-b5ce7a3b21f7>
2010-07-29 21:18:35 +0000

    fixed float formatting

--- trunk/src/coid/comm/comm_test/main.cpp
+++ trunk/src/coid/comm/comm_test/main.cpp
@@ -1,4 +1,5 @@
 
+#include "../str.h"
 
 namespace coid {
 void test();
@@ -9,9 +10,25 @@ int main_atomic(int argc, char * argv[]);
 
 void regex_test();
 
+void float_test()
+{
+    char buf[32];
+    double v = 12345678.90123456789;
+    int n = 8;
+    int nfrac = 3;
+
+    while(1)
+    {
+        ::memset(buf, 0, 32);
+        coid::charstrconv::append_float(buf, buf+n, v, nfrac);
+    }
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 int main( int argc, char* argv[] )
 {
+	float_test();
+
     //main_atomic(argc, argv);
     //coid::test();
     coid::metastream_test();
