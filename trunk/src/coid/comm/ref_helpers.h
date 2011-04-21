b4c2696ee2dcdf3c227aeae35820ac56ca016c4f
laco.hrabcak@gmail.com <laco.hrabcak@gmail.com@d0b4f782-2c5c-11df-af82-b5ce7a3b21f7>
2011-04-21 10:39:41 +0000

    fixed queue/list clear...

--- trunk/src/coid/comm/ref_helpers.h
+++ trunk/src/coid/comm/ref_helpers.h
@@ -6,29 +6,36 @@
 
 namespace coid {
 
+// takeover
+
 template<class T>
 struct queue_helper_trait {
     static void take(T& dst,T& src) { dst=src; }
+    static void release(T& item) {}
 };
 
 template<class K>
 struct queue_helper_trait<ref<K>> {
     static void take(ref<K>& dst,ref<K>& src) { dst.takeover(src); }
+    static void release(ref<K>& item) { item.release(); }
 };
 
 template<class K>
 struct queue_helper_trait<iref<K>> {
     static void take(iref<K>& dst,iref<K>& src) { dst.takeover(src); }
+    static void release(iref<K>& item) { item.release(); }
 };
 
 template<>
 struct queue_helper_trait<coid::charstr> {
     static void take(coid::charstr &dst,coid::charstr &src) { dst.takeover(src); }
+    static void release(coid::charstr& item) { item.reset(); }
 };
 
 template<class K>
 struct queue_helper_trait<coid::dynarray<K>> {
-    static void take(coid::dynarray<K> &dst,coid::dynarray<K> &src) { dst.takeover(src); }
+    static void take(coid::dynarray<K>& dst,coid::dynarray<K>& src) { dst.takeover(src); }
+    static void release(coid::dynarray<K>& item) { item.reset(); }
 };
 
 } // end of namespace coid
