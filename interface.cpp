86f24190d097ee33996be98f142b77f5d504abca
cameni <cameni@d0b4f782-2c5c-11df-af82-b5ce7a3b21f7>
2013-08-22 08:36:20 +0000

    removed comm/interface dependencies on mutex and hash

--- interface.cpp
+++ interface.cpp
@@ -1,14 +1,34 @@
 
 #include "interface.h"
 #include "commexception.h"
+#include "hash/hashkeyset.h"
+#include "sync/mutex.h"
 
 COID_NAMESPACE_BEGIN
 
+struct entry
+{
+    token ifcname;
+    void* creator;
+
+    operator const token&() const { return ifcname; }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+class interface_register_impl : public interface_register
+{
+public:
+
+    hash_keyset<entry, _Select_GetRef<entry,token> > _hash;
+    comm_mutex _mx;
+
+    static interface_register_impl& get();
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 void* interface_register::get_interface_creator( const token& ifcname )
 {
-    interface_register& reg = interface_register::get();
+    interface_register_impl& reg = interface_register_impl::get();
     GUARDTHIS(reg._mx);
 
     const entry* en = reg._hash.find_value(ifcname);
@@ -19,7 +39,7 @@ void* interface_register::get_interface_creator( const token& ifcname )
 ////////////////////////////////////////////////////////////////////////////////
 void interface_register::register_interface_creator( const token& ifcname, void* creator )
 {
-    interface_register& reg = interface_register::get();
+    interface_register_impl& reg = interface_register_impl::get();
     GUARDTHIS(reg._mx);
 
     entry* en = reg._hash.insert_value_slot(ifcname);
@@ -49,15 +69,15 @@ __declspec(dllimport) proc_t __stdcall GetProcAddress (
     const char* procname
     );
 
-typedef interface_register* (*ireg_t)();
+typedef interface_register_impl* (*ireg_t)();
 
 #define MAKESTR(x) STR(x)
 #define STR(x) #x
 
 ////////////////////////////////////////////////////////////////////////////////
-interface_register& interface_register::get()
+interface_register_impl& interface_register_impl::get()
 {
-    static interface_register* _this=0;
+    static interface_register_impl* _this=0;
 
     if(!_this) {
         const char* s = MAKESTR(INTERGEN_GLOBAL_REGISTRAR);
@@ -70,22 +90,22 @@ interface_register& interface_register::get()
 }
 
 
-extern "C" __declspec(dllexport) interface_register* INTERGEN_GLOBAL_REGISTRAR()
+extern "C" __declspec(dllexport) interface_register_impl* INTERGEN_GLOBAL_REGISTRAR()
 {
-    return &SINGLETON(interface_register);
+    return &SINGLETON(interface_register_impl);
 }
 
 
 #else
 /*
-extern "C" __attribute__ ((visibility("default"))) interface_register* INTERGEN_GLOBAL_REGISTRAR();
+extern "C" __attribute__ ((visibility("default"))) interface_register_impl* INTERGEN_GLOBAL_REGISTRAR();
 {
-    return &SINGLETON(interface_register);
+    return &SINGLETON(interface_register_impl);
 }*/
 
-interface_register& interface_register::get()
+interface_register_impl& interface_register_impl::get()
 {
-    static interface_register _this;
+    static interface_register_impl _this;
 
     return _this;
 }
