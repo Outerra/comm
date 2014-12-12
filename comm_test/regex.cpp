1f0c0dc9602645c97d49bd25a478e28e22405bcf
laco.hrabcak <laco.hrabcak@d0b4f782-2c5c-11df-af82-b5ce7a3b21f7>
2014-12-12 14:42:48 +0000

    fixed include paths for chromium integration added new gyp for c5e

--- comm_test/regex.cpp
+++ comm_test/regex.cpp
@@ -1,7 +1,7 @@
 
-#include "comm/regex.h"
-#include "comm/dynarray.h"
-#include "comm/commassert.h"
+#include "../regex.h"
+#include "../dynarray.h"
+#include "../commassert.h"
 
 using namespace coid;
 
