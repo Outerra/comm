e72ac1f9175b16f0b94db17178d411f28e291ce8
Laco Hrabcak <laco.hrabcak@gmail.com>
2015-06-17 09:04:09 +0200

    added typedefs to alloc_2d

--- alloc/alloc2d.h
+++ alloc/alloc2d.h
@@ -18,6 +18,8 @@ public:
 
     class node;
     typedef T type_t;
+    typedef uint handle_t;
+    typedef uint2 handle2_t;
 
 private:
 
@@ -27,23 +29,20 @@ private:
         slotalloc_bmp<node> _nodes;
 
     public:
-	    uint new_node(const type_t &pos, const type_t &size, const uint parent)
+        handle_t new_node(const type_t &pos, const type_t &size, const handle_t parent)
         {
             node * const n = new (_nodes.add_uninit()) node(pos, size, parent);
-            const uint id = _nodes.get_item_id(n);
+            const handle_t id = _nodes.get_item_id(n);
             return id;
         }
 	
-        void delete_node(const uint id)
-        {
-            _nodes.del(id);
-        }
+        void delete_node(const handle_t id) { _nodes.del(id); }
 
-        node* ptr(const uint id) { return _nodes.get_item(id); }
+        node* ptr(const handle_t id) { return _nodes.get_item(id); }
 
-        uint id(const node* const ptr) const { return _nodes.get_item_id(ptr); }
+        handle_t id(const node* const ptr) const { return _nodes.get_item_id(ptr); }
 
-        uint next(const uint id) const { return _nodes.next(id); }
+        handle_t next(const handle_t id) const { return _nodes.next(id); }
     };
 
 public:
@@ -55,8 +54,8 @@ public:
     private:
 	    type_t _pos;
 	    type_t _size;
-	    uint2 _child;
-	    uint _parent;
+	    handle2_t _child;
+        handle_t _parent;
         uint _flags;
     
     public:
@@ -64,36 +63,37 @@ public:
 
         const type_t& get_pos() const { return _pos; }
         const type_t& get_size() const { return _size; }
-        bool is_leaf() const { return _child[0] == -1 && _child[1] == -1; }
+        bool is_leaf() const { return _child[0] == UMAX32 && _child[1] == UMAX32; }
         bool has_data() const { return _flags != 0; }
+        bool can_release() const { return is_leaf() && !has_data(); }
 
     protected:
 
-	    node(const type_t &pos, const type_t &size, const uint parent)
+        node(const type_t &pos, const type_t &size, const handle_t parent)
 		    : _pos(pos)
 		    , _size(size)
-            , _child(-1)
+            , _child(UMAX32)
 		    , _parent(parent)
             , _flags(0)
             , _data()
         {}
 
-	    static uint insert(const uint id, const type_t &size, node_pool &pool)
+        static handle_t insert(const handle_t id, const type_t &size, node_pool &pool)
         {
             node * const n = pool.ptr(id);
-            uint2 _child = n->_child;
+            handle2_t _child = n->_child;
             const T _size = n->_size;
             const uint _flags = n->_flags;
 
             if (!pool.ptr(id)->is_leaf()) {
-                uint n = insert(_child[0], size, pool);
-                if (n == -1)
+                handle_t n = insert(_child[0], size, pool);
+                if (n == UMAX32)
                     n = insert(_child[1], size, pool);
                 return n;
 		    }
 		    else {
                 if (_flags != 0 || size.x > _size.x || size.y > _size.y)
-                    return -1;
+                    return UMAX32;
 
                 if (size.x == _size.x && size.y == _size.y) {
                     pool.ptr(id)->_flags = 1;
@@ -106,11 +106,11 @@ public:
 		    }
 	    }
 
-	    static void divide(const uint id, const type_t &size, node_pool &pool, int &depth)
+        static void divide(const handle_t id, const type_t &size, node_pool &pool, int &depth)
         {
             node * n = pool.ptr(id);
             const T _size = n->_size;
-            uint2 _child = n->_child;
+            handle2_t _child = n->_child;
 
             if (!n->is_leaf()) {
                 divide(_child[0], size, pool, depth);
@@ -132,14 +132,14 @@ public:
 		    }
 	    }
 
-	    static uint2 split(const uint id, const T &size, node_pool &pool)
+        static handle2_t split(const handle_t id, const T &size, node_pool &pool)
         {
             node * n = pool.ptr(id);
             const T _pos = n->_pos;
             const T _size = n->_size;
             const type_t d = _size - size;
 
-            uint2 child;
+            handle2_t child;
             if (d.x > d.y) {
 			    child[0] = pool.new_node(_pos, type_t(size.x, _size.y), id);
 			    child[1] = pool.new_node( type_t(_pos.x + size.x, _pos.y), type_t(_size.x - size.x, _size.y), id);
@@ -158,14 +158,14 @@ public:
 private:
 
     node_pool _node_pool;
-	uint const _root;
+    handle_t const _root;
 	const int _initial_split_size;
 
 public:
 
 	alloc_2d(const int size, const int initial_split_size)
 		: _node_pool()
-		, _root(_node_pool.new_node(type_t(0), type_t(size), -1))
+        , _root(_node_pool.new_node(type_t(0), type_t(size), UMAX32))
 		, _initial_split_size(initial_split_size)
 	{
 		if(initial_split_size > 0) {
@@ -177,47 +177,43 @@ public:
 	~alloc_2d() {}
 
 	/// return position in virtual 2D space
-	uint alloc_space(const T &size)
+    handle_t alloc_space(const T &size)
     { 
-        const uint id = node::insert(_root, size, _node_pool);
-
-        //DASSERT(_node_pool.ptr(id)->has_data());
-
+        const handle_t id = node::insert(_root, size, _node_pool);
         return id;
 	}
 
 	///
-	void free_space(uint id, const bool leaf = true)
+    void free_space(handle_t id, const bool leaf = true)
     {
         node * const n = _node_pool.ptr(id);
 
-        const uint parent_id = n->_parent;
+        const handle_t parent_id = n->_parent;
 
-        if (parent_id != -1) {
+        if (parent_id != UMAX32) {
             node * const parent = _node_pool.ptr(n->_parent);
-            node * const child0 = _node_pool.ptr(parent->_child.x);
-            node * const child1 = _node_pool.ptr(parent->_child.y);
+            const node * const child0 = _node_pool.ptr(parent->_child.x);
+            const node * const child1 = _node_pool.ptr(parent->_child.y);
 
             if (leaf)
                 n->_flags = 0; // clean data flag
 
-            if (child0->is_leaf() && child1->is_leaf()
-                && !child0->has_data() && !child1->has_data()) {
+            if (child0->can_release() && child1->can_release()) {
                 _node_pool.delete_node(parent->_child.x);
                 _node_pool.delete_node(parent->_child.y);
-                parent->_child.x = parent->_child.y = -1;
+                parent->_child.x = parent->_child.y = UMAX32;
                 free_space(parent_id, false);
             }
         }
 	}
 
     ///
-    D* get_ptr(const uint id) { return &_node_pool.ptr(id)->_data; }
+    D* get_ptr(const handle_t id) { return &_node_pool.ptr(id)->_data; }
 
     ///
-    uint get_next(const uint id) const { return _node_pool.next(id); }
+    handle_t get_next(const handle_t id) const { return _node_pool.next(id); }
 
-    const node* get_node_ptr(uint id) { return _node_pool.ptr(id); }
+    const node* get_node_ptr(handle_t id) { return _node_pool.ptr(id); }
 };
 
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
