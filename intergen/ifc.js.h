063532154b9e73ee1564fc80e27e7733dd99a6af
cameni <cameni@users.noreply.github.com>
2018-04-04 13:53:12 +0200

    fixed _generic_interface_creator in MT interface queries

--- intergen/ifc.js.h
+++ intergen/ifc.js.h
@@ -347,6 +347,9 @@ struct interface_context
         if (_object.IsEmpty())
             return V8_CUR_CONTEXT(iso);
 
+        if (_object.IsEmpty())
+            return V8_CUR_CONTEXT(iso);
+
 #ifdef V8_MAJOR_VERSION
         return _object.Get(iso)->CreationContext();
 #else
