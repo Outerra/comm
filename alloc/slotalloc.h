5b6618f0fc1c91491bb3827386920ea265753ab3
Brano Kemen <cameni@gmail.com>
2018-09-22 09:22:37 +0200

    v8 v7.0

--- alloc/slotalloc.h
+++ alloc/slotalloc.h
@@ -52,6 +52,10 @@
 
 COID_NAMESPACE_BEGIN
 
+#if defined(COID_CONSTEXPR_IF) && !defined(__cpp_if_constexpr)
+#error Please enable C++17 language standard (/std:c++17) in project settings for VS2017+ projects
+#endif
+
 ////////////////////////////////////////////////////////////////////////////////
 /**
 @brief Allocator for efficient slot allocation/deletion of objects.
