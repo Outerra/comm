ee19efd97ff13a6aa94f75a8ec54282f3c1bc9bc
cyrilgramblicka <cyril.gramblicka@gmail.com>
2022-03-26 08:10:20 +0100

    dir drive trailing separator fix

--- dirwin.cpp
+++ dirwin.cpp
@@ -86,14 +86,16 @@ opcd directory::open( const token& path, const token& filter )
     _pattern = path;
     if(_pattern.len() > 1  &&  _pattern.len() <= 3  &&  _pattern.nth_char(1) == ':')
         treat_trailing_separator(_pattern, true);
+
     else if( _pattern.last_char() == '\\' || _pattern.last_char() == '/' )
         _pattern.resize(-1);
 
-    if(_stat64(_pattern.ptr(), &_st) != 0)
+    if(_stat64(_pattern.ptr(), &_st) != 0 || (_st.st_mode & _S_IFDIR) == 0)
         return ersFAILED;
 
-    if(_pattern.last_char() != '\\')
+    if (_pattern.last_char() != '\\' && _pattern.last_char() != '/')
         _pattern << '\\';
+
     _curpath = _pattern;
     _baselen = _curpath.len();
     _pattern << (filter ? filter : token("*.*"));
