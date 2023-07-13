aeb300f0181f4413699dec020dbbdd5723a4e826
Jakub Lukasik <lukasik.lukasik@gmail.com>
2023-07-13 14:43:11 +0200

    unique ptr

---
+++ unique_ptr.h
@@ -0,0 +1,133 @@
+#ifndef __COID_COMM_UNIQUE__HEADER_FILE__
+#define __COID_COMM_UNIQUE__HEADER_FILE__
+
+#include "namespace.h"
+
+#include "commtypes.h"
+#include "binstream/binstream.h"
+//#include <memory>
+
+
+COID_NAMESPACE_BEGIN
+
+//Forward declarations
+template<class T> class unique_ptr;
+template<class T> binstream& operator << (binstream&, const unique_ptr<T>&);
+template<class T> binstream& operator >> (binstream&, unique_ptr<T>&);
+
+///Template making pointers uniquely owned, and to be automatically destructed when the owner is destructed
+template <class T>
+class unique_ptr
+{
+    T* _p = nullptr;
+public:
+    unique_ptr() {}
+    ~unique_ptr() { if (_p) { delete _p; } }
+
+    unique_ptr(T* p) { _p = p; }
+
+    unique_ptr(const unique_ptr& p) = delete;
+
+    //unique_ptr(unique_ptr&& p)
+    //{
+    //    _p = p._p;
+    //    p._p = nullptr;
+    //}
+
+    template <class U>
+    unique_ptr(unique_ptr<U>&& p)
+    {
+        _p = p.eject();
+    }
+
+    template <typename... Args>
+    static unique_ptr<T> create(Args&&... args) {
+        return unique_ptr<T>(new T(static_cast<Args&&>(args)...));
+    }
+
+    T* ptr() const { return _p; }
+
+    //operator T* (void) const { return _p; }
+
+    int operator ==(const T* ptr) const { return ptr == _p; }
+    int operator !=(const T* ptr) const { return ptr != _p; }
+
+    T& operator *(void) { return *_p; }
+    const T& operator *(void) const { return *_p; }
+
+    #ifdef SYSTYPE_MSVC
+    #pragma warning (disable : 4284)
+    #endif //SYSTYPE_MSVC
+    T* operator ->(void) { return _p; }
+    const T* operator ->(void) const { return _p; }
+    #ifdef SYSTYPE_MSVC
+    #pragma warning (default : 4284)
+    #endif //SYSTYPE_MSVC
+
+    unique_ptr& operator = (const T* p) {
+        if (_p) delete _p;
+        _p = const_cast<T*>(p);
+        return *this;
+    }
+
+    unique_ptr& operator = (unique_ptr&& p)
+    {
+        if (_p) {
+            delete _p;
+        }
+        swap(p);
+        return *this;
+    }
+
+    T*& get_ptr() const { return (T*&)_p; }
+
+    friend binstream& operator << <T>(binstream& out, const unique_ptr<T>& ptr);
+    friend binstream& operator >> <T>(binstream& in, unique_ptr<T>& ptr);
+
+    bool is_set() const { return _p != nullptr; }
+
+    T* eject() { T* tmp = _p; _p = nullptr; return tmp; }
+
+    unique_ptr&& move() { return static_cast<unique_ptr&&>(*this); }
+
+    void destroy()
+    {
+        if (_p)
+            delete _p;
+        _p = nullptr;
+    }
+
+    void swap(unique_ptr<T>& other)
+    {
+        std::swap(_p, other._p);
+    }
+
+    friend void swap(unique_ptr& a, unique_ptr& b) {
+        std::swap(a._p, b._p);
+    }
+};
+
+template <class T>
+binstream& operator << (binstream& out, const unique_ptr<T>& ptr) {
+    if (ptr.is_set())
+        out << (uint)1;
+    else
+        out << (uint)0x00;
+    if (ptr._p)  out << *ptr._p;
+    return out;
+}
+
+template <class T>
+binstream& operator >> (binstream& in, unique_ptr<T>& ptr) {
+    uint is;
+    in >> is;
+    if (is) {
+        ptr._p = new T;
+        in >> *ptr._p;
+    }
+    return in;
+}
+
+COID_NAMESPACE_END
+
+#endif // __COID_COMM_UNIQUE__HEADER_FILE__
